937234d30423c988a484e415c80b99736f29cb10
KevinOh45 <61233166+kevinoh45TuD@users.noreply.github.com>
2020-12-22 18:57:37 +0000

    car

--- Night-Drive-Build/Assets/Scripts/BlocksEnable.cs
+++ Night-Drive-Build/Assets/Scripts/BlocksEnable.cs
@@ -7,18 +7,17 @@ using Random = UnityEngine.Random;
 public class BlocksEnable : MonoBehaviour
 {
     public int optionNum;
-    public int optionAmount;
+    
 
     public GameObject[] blockSet;
     
     void OnEnable()
     {
-        optionAmount = Random.Range(0, 3);
+        
         optionNum = Random.Range(0, blockSet.Length);
 
-        for (int i = 0; i < optionAmount; i++)
-        {
-            blockSet[optionNum].SetActive(true);
-        }
+        
+        blockSet[optionNum].SetActive(true);
+        
     }
 }
--- Night-Drive-Build/Assets/Scripts/carNow.cs
+++ Night-Drive-Build/Assets/Scripts/carNow.cs
@@ -13,15 +13,32 @@ public class carNow : MonoBehaviour
     //public GameObject roadParent;
 
     public GameObject thisCar;
+    
+    private Vector3 currentEulerAngles;
+    private Quaternion currentRotation;
+
+    public float dropAmount;
 
     private void OnEnable()
     {
+        currentEulerAngles += new Vector3(0, 180, 0);
+        
+        currentRotation.eulerAngles = currentEulerAngles;
+        
         Destroy(gameObject);
 
-        whichVehicle = Random.Range(0, vehicleObj.Length);
+        whichVehicle = Random.Range(0, 10);
 
-        thisCar = Instantiate(vehicleObj[whichVehicle], transform.position, transform.rotation, transform.parent);
-        
-        thisCar.transform.localScale = new Vector3(0.1f, 2f, 0.1f);
+        if (whichVehicle <= 9)
+        {
+            thisCar = Instantiate(vehicleObj[0], transform.position, transform.rotation, transform.parent);
+        }
+        else if (whichVehicle >= 10)
+        {
+            thisCar = Instantiate(vehicleObj[1], transform.position, transform.rotation, transform.parent);
+        }
+        thisCar.transform.rotation =  currentRotation;
+        thisCar.transform.position -= new Vector3(0,dropAmount,0);
+        thisCar.transform.localScale = new Vector3(0.05f, 2f, 0.05f);
     }
 }
