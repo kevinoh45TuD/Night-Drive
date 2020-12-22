5f7856bf6e7870e01807f57344710563830a7c81
KevinOh45 <61233166+kevinoh45TuD@users.noreply.github.com>
2020-12-22 18:19:41 +0000

    car change

---
+++ Night-Drive-Build/Assets/Scripts/carNow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class carNow : MonoBehaviour
+{
+    public GameObject[] vehicleObj;
+
+    public int whichVehicle;
+
+    //public GameObject roadParent;
+
+    public GameObject thisCar;
+
+    private void OnEnable()
+    {
+        Destroy(gameObject);
+
+        whichVehicle = Random.Range(0, vehicleObj.Length);
+
+        thisCar = Instantiate(vehicleObj[whichVehicle], transform.position, transform.rotation, transform.parent);
+        
+        thisCar.transform.localScale = new Vector3(0.1f, 2f, 0.1f);
+    }
+}
